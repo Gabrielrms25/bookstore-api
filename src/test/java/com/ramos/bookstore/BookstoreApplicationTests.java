81fcdcbb49043c55de10ba3743970d9c324a8021
Gabrielrms25 <ramosbrasil25@gmail.com>
2021-07-19 21:38:20 -0300

    Commit inicial

---
+++ src/test/java/com/ramos/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramos.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
