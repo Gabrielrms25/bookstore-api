a8d5636099555801efd4e80f5129b1e37d41d9c9
Gabrielrms25 <ramosbrasil25@gmail.com>
2021-07-28 20:47:02 -0300

    POST para livros

--- src/main/java/com/ramos/bookstore/service/LivroService.java
+++ src/main/java/com/ramos/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ramos.bookstore.domain.Categoria;
 import com.ramos.bookstore.domain.Livro;
 import com.ramos.bookstore.repositories.LivroRepository;
 import com.ramos.bookstore.service.exceptions.ObjectNotFoundException;
@@ -43,6 +44,14 @@ public class LivroService {
 		newObj.setTitulo(obj.getTitulo());
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
+	}
+
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return repository.save(obj);
 	}
 	
 }
