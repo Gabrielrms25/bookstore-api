9464f53fb97af779ea00ded960006aabac587f1d
Gabrielrms25 <ramosbrasil25@gmail.com>
2021-07-28 19:39:24 -0300

    Listando todos os Livros por categoria

--- src/main/java/com/ramos/bookstore/resource/LivroResource.java
+++ src/main/java/com/ramos/bookstore/resource/LivroResource.java
@@ -1,13 +1,18 @@
 package com.ramos.bookstore.resource;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ramos.bookstore.domain.Livro;
+import com.ramos.bookstore.dto.LivroDTO;
 import com.ramos.bookstore.service.LivroService;
 
 @RestController
@@ -24,5 +29,12 @@ public class LivroResource {
 		return ResponseEntity.ok().body(obj);
 	}
 		
+	@GetMapping(value = "")
+	public ResponseEntity<List<LivroDTO>> findAll(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat){
+		List<Livro> list = livroService.findAll(id_cat);
+		List<LivroDTO> listDTO = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+		//localhost:8080/livros?/categoria=1
+	}
 	
 }
--- src/main/java/com/ramos/bookstore/service/LivroService.java
+++ src/main/java/com/ramos/bookstore/service/LivroService.java
@@ -16,18 +16,21 @@ public class LivroService {
 	@Autowired
 	private LivroRepository repository;
 	
+	
+	@Autowired
+	private CategoriaService categoriaService;
+	
 	public Livro findById(Integer id) {
 		Optional<Livro> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + id + ", Tipo: " + Livro.class.getName()));
+				"Livro não encontrado! Id: " + id + ", Tipo: " + Livro.class.getName()));
 	}
 	
 	
-	public List<Livro> findAll(){
-		return repository.findAll();
+	public List<Livro> findAll(Integer id_cat){
+		categoriaService.findById(id_cat);
+		return repository.findAllByCategoria(id_cat);
 	}
-	
-	
 	
 	
 }
