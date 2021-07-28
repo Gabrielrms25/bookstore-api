7c185508e15440e5e12343f005c84aa6623b6f5a
Gabrielrms25 <ramosbrasil25@gmail.com>
2021-07-28 19:17:48 -0300

    GET para livro por id

---
+++ src/main/java/com/ramos/bookstore/service/LivroService.java
@@ -0,0 +1,33 @@
+package com.ramos.bookstore.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ramos.bookstore.domain.Livro;
+import com.ramos.bookstore.repositories.LivroRepository;
+import com.ramos.bookstore.service.exceptions.ObjectNotFoundException;
+
+@Service
+public class LivroService {
+
+	@Autowired
+	private LivroRepository repository;
+	
+	public Livro findById(Integer id) {
+		Optional<Livro> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Livro.class.getName()));
+	}
+	
+	
+	public List<Livro> findAll(){
+		return repository.findAll();
+	}
+	
+	
+	
+	
+}
