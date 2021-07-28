8ffc39240ad0ed3c6e87e94a54037e36adadd4a4
Gabrielrms25 <ramosbrasil25@gmail.com>
2021-07-28 01:38:54 -0300

    Excecao personalizada para delete de Categoria

--- src/main/java/com/ramos/bookstore/service/CategoriaService.java
+++ src/main/java/com/ramos/bookstore/service/CategoriaService.java
@@ -9,40 +9,47 @@ import org.springframework.stereotype.Service;
 import com.ramos.bookstore.domain.Categoria;
 import com.ramos.bookstore.dto.CategoriaDTO;
 import com.ramos.bookstore.repositories.CategoriaRepository;
+import com.ramos.bookstore.service.exceptions.DataIntegrityViolationException;
 import com.ramos.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repository;
-	
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
-	public List<Categoria> findAll(){
+
+	public List<Categoria> findAll() {
 		return repository.findAll();
-		
+
 	}
-	
+
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
 		return repository.save(obj);
 	}
-	
+
 	public Categoria update(Integer id, CategoriaDTO objDTO) {
 		Categoria obj = findById(id);
 		obj.setNome(objDTO.getNome());
 		obj.setDescricao(objDTO.getDescricao());
 		return repository.save(obj);
 	}
-	
+
 	public void deleteById(Integer id) {
-		findById(id);
-		repository.deleteById(id);
+		repository.findById(id);
+
+		try {
+			repository.deleteById(id);
+		} catch (org.springframework.dao.DataIntegrityViolationException e) {
+			throw new DataIntegrityViolationException(
+					"Categoria nao pode ser removida,possui livros associados!");
+		}
 	}
-	
+
 }
